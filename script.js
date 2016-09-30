d9e7f5aacc6ce98bade864e4eed167710bebe7bd
Hi <Hi@MollydeMacBook-Air.local>
2016-09-30 12:00:54 -0400

    MollyWork

--- script.js
+++ script.js
@@ -9,9 +9,32 @@
  * and add them to the "productList" array using array.push()
  */
 
+var shoe1 = {
+	name:"Venture",
+	price: 79.95,
+	maker: "Gel"
+};
+var shoe2 = {
+	name:"Unisex",
+	price: 32.99,
+	maker: "Jackshibo"
+}
+var shoe3 = {
+	name:"Gusto Cross",
+	price: 39.97,
+	maker: "Champion"
+}
+var shoe4 = {
+	name:"Suede Classic",
+	price: 168.32,
+	maker: "PUMA"
+}
+
 var productList = []; //this is an array
 
 //Your code here
+
+productList.push(shoe1, shoe2, shoe3, shoe4);
 //Hint: productList.push(newObject) will add the object you just created to the array
 
 console.log(productList[0]);
@@ -24,13 +47,25 @@ console.log(productList[0]);
 for(var i = 0; i < productList.length; i++){
 
     //Your code here
+    if (productList[i].price< 50){
 
-    console.log();
+    console.log("The product" + productList[i].name + "costs" + productList[i].price + ", and is a match");
 }
-
+}
 /* Step 3
  * What is the average price of all the products?
  */
+var sum = 0;
+
+    for(var i = 0; i < productList.length; i++){
+
+    	sum = sum + productList[i].price 
+
+    }  
+
+var average = sum/productList.length
+
+console.log(average);
 
 //3.1 first, calculate total price using the for loop again
 
@@ -45,6 +80,12 @@ for(var i = 0; i < productList.length; i++){
 function comparePrice(p){
 
     var numOfMatchingProducts = 0;
+    for(var i =0; i<productList.length;i++){
+
+    if(productList[i].price < p ){
+    	numOfMatchingProducts ++
+    }
+    }
 
     //Your code here
 
